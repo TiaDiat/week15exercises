6c871813adca0fab6846f36ecd5bf09310ebbbbe
Ekaterina Diatchenko <t1ad1at@gmail.com>
2018-08-13 20:07:48 -0700

    almost complete

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//store a new Vue instance in a variable called “todo”
+var todo = new Vue ({
+	el: '#todo',
+	data: {
+		newTask:'',
+		todoList:[]
+	},
+	methods: {
+		addTask: function() {
+			var task = this.newTask;
+			this.todoList.push(task);
+			this.newTask = '';
+		},
+		removeTask: function(task) {
+			var index = this.todoList.indexOf(task);
+			this.todoList.splice(index, 1);
+		}
+	}
+});
